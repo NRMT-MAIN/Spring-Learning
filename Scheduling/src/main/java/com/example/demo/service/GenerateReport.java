d8a6355938784ba5e1e38438ec17ffe28e298593
NRMT-MAIN <sahunirmit.main@gmail.com>
2025-10-05 20:42:23 +0530

    Implemented Some Cron Job

--- Scheduling/src/main/java/com/example/demo/service/GenerateReport.java
+++ Scheduling/src/main/java/com/example/demo/service/GenerateReport.java
@@ -12,13 +12,29 @@ public class GenerateReport {
 	//@Scheduled(fixedDelay = 3000)
 	//@Scheduled(fixedDelayString = "3000")
 	//@Scheduled(fixedDelay = 3000)
-	@Scheduled(fixedRate = 3000)
-	public void showReport() {
-		System.out.println("report on :: " + new Date());
-		try {
-			Thread.sleep(15000);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+//	@Scheduled(fixedRate = 3000)
+//	public void showReport() {
+//		System.out.println("report on :: " + new Date());
+//		try {
+//			Thread.sleep(15000) ;
+//		} catch (Exception e) {
+//			e.printStackTrace() ;
+//		}
+//	}
+	
+//	@Scheduled(fixedDelay = 3000)
+//	public void showReport1() {
+//		System.out.println("GenerateReport.showReport1() :: " + new Date() + " ---- " + Thread.currentThread());
+//	}
+//	
+//	@Scheduled(fixedDelay = 4000)
+//	public void showReport2() {
+//		System.out.println("GenerateReport.showReport2() :: " + new Date() + " ---- " + Thread.currentThread());
+//	}
+	
+	//@Scheduled(cron = "20 * * * * *")
+	@Scheduled(cron = "0 20/10 * * * *")
+	public void generateReport1() {
+		System.out.println("GenerateReport.generateReport1() :: " + new Date());
 	}
 }
