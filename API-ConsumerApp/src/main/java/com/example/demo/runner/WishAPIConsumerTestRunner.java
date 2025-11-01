35ebc10ef9d8c07d186cda762dafdc75fde42dd9
NRMT-MAIN <sahunirmit.main@gmail.com>
2025-11-01 23:44:19 +0530

    Added RestTemplate Producer-Consumer

---
+++ API-ConsumerApp/src/main/java/com/example/demo/runner/WishAPIConsumerTestRunner.java
@@ -0,0 +1,29 @@
+package com.example.demo.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class WishAPIConsumerTestRunner implements CommandLineRunner {
+	private static final String SERVICE_URL = 
+	        "http://localhost:3030/wish-api/wish"; 
+	
+	@Override
+	public void run(String... args) throws Exception {
+		RestTemplate template = new RestTemplate() ; 
+		
+		ResponseEntity<String> res = template.getForEntity(SERVICE_URL, String.class) ; 
+		
+		System.out.println("\n--- Response Details (getForEntity) ---");
+        System.out.println("Response body (Output) :: " + res.getBody());
+        System.out.println("Response Status Code :: " + res.getStatusCode());
+        System.out.println("Response Status Code Value :: " + res.getStatusCodeValue());
+        
+        String resultBody = template.getForObject(SERVICE_URL, String.class);
+        System.out.println("\n--- Response Details (getForObject) ---");
+        System.out.println("Result (Body Only) :: " + resultBody);
+		
+	}
+}
